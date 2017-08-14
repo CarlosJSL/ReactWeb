52334acd0ba29ef6b4265d289a54cdded191d9e9
CarlosJSL <carlosjleonel@hotmai.com>
2017-08-13 21:32:18 -0300

    Initializing the template

---
+++ src/about/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+        <div> 
+            <PageHeader name = 'Sobre' small = 'Nós' ></PageHeader>
+            <h2> Nossa História </h2>
+            <p> creu</p>
+            <h2>Missao e visao </h2>
+            <p> creu</p>
+        </div>
+)
